91a83e568720b5d057ff86dbcbe85f6f05e05ec6
ZiaCodes <ziarzai@hotmail.com>
2024-08-27 12:24:20 +0500

    feat: installed framer motion

--- components/hero-circle.tsx
+++ components/hero-circle.tsx
@@ -5,7 +5,7 @@ export default function HeroCircle() {
       <div className="absolute top-[-120px] left-1/2 transform -translate-x-1/2 z-20 blur-xl w-[584px] h-[103px] mix-blend-plus-lighter bg-secondary/45 rounded-full shadow-[0_0_200px_rgba(255,255,255,0.1)]" />
 
       {/* Gray circle with box shadow */}
-      <div className="absolute top-0 z-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 size-[1071px] bg-[#040404] rounded-full shadow-[0_0_200px_rgba(255,255,255,0.1)]"></div>
+      <div className="absolute top-0 z-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 size-[1071px] bg-[#040404] rounded-full shadow-[0_0_200px_rgba(255,255,255,0.1)]" />
     </>
   )
 }
