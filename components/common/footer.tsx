fdcd0c250bfeb109c1637595000bd6ec654e1fe7
ZiaCodes <ziarzai@hotmail.com>
2024-08-27 11:49:41 +0500

    fix: footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer className="py-5 bg-primary text-white/60 border-t border-white/20">
-      <div className="container text-center">
+      <div className="container w-full mx-auto flex items-center justify-center text-center">
         Brought to you{' '}
         <Link href="https://zzia.vercel.app" target="_blank" className="underline underline-offset-4 hover:text-white">
           Zia Ur Rehman
