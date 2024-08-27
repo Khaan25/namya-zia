234e27e3b3b42b182dca8c34860a8bd4a9204957
ZiaCodes <ziarzai@hotmail.com>
2024-08-27 11:51:50 +0500

    fix: spacing

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="py-5 bg-primary text-white/60 border-t border-white/20">
       <div className="container w-full mx-auto flex items-center justify-center text-center">
-        Brought to you{' '}
+        Brought to you{'  '}
         <Link href="https://zzia.vercel.app" target="_blank" className="underline underline-offset-4 hover:text-white">
           Zia Ur Rehman
         </Link>
